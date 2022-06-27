27a3206121fdaf98b72fe7e08cbdeb415827e328
신승훈 <zlzlzlmo@daum.net>
2022-06-28 07:19:57 +0900

    text 컴포넌트 대신 view에 border-radius적용

--- App.tsx
+++ App.tsx
@@ -13,11 +13,15 @@ import {
 // SafeAreaView는 아이폰에서 Status Bar 크기 만큼 적용시켜서 padding top같은 역할을 해주는 api
 // 안드로이드에는 적용이 안되기 때문에 Platform과 StatusBar.currentHeight를 사용하여 paddingTop적용
 
+// react와 달리 RN에서는 onChangeText (react에서는 onChange )와 onPress (react에서는 onClick ) 이벤트를 사용한다
+
+// 아이폰에서는 Text에 border-radius가 들어가질 않기떄문에 View 컴포넌트로 Text를 감싸고 그 View컴포넌트에다가 border-radius를 줘야한다!!!
+// 크로스플랫폼 특성상 일반적으로 아이폰과 안드로이드에서 같은 코드로 똑같이 작용되는 경우가 있지만 가끔씩 다르게 작동되는 부분들이 있다. 그 부분중 하나가 text border-radius이다.
+
 export default function App() {
   const [enteredGoalText, setEnteredGoalText] = useState<string>("");
   const [goals, setGoals] = useState<string[]>([]);
 
-  // react와 달리 RN에서는 onChangeText (react에서는 onChange )와 onPress (react에서는 onClick ) 이벤트를 사용한다
   const handleGoalInput = (text: string) => {
     setEnteredGoalText(text);
   };
@@ -39,7 +43,9 @@ export default function App() {
         </View>
         <View style={styles.goalsContainer}>
           {goals.map((goal, index) => (
-            <Text>{goal}</Text>
+            <View style={styles.goalItem} key={index}>
+              <Text style={styles.goalText}>{goal}</Text>
+            </View>
           ))}
         </View>
       </View>
@@ -75,5 +81,14 @@ const styles = StyleSheet.create({
   },
   goalsContainer: {
     flex: 4,
+  },
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: "#5e0acc",
+  },
+  goalText: {
+    color: "#fff",
   },
 });
