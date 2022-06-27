044950edbc8d9ed1adb0c2c57b1ebe584a3824ef
신승훈 <zlzlzlmo@daum.net>
2022-06-28 06:52:43 +0900

    SafeAreaView와 Platform Api를 사용하여 Status bar 만큼 padding top 적용

--- App.tsx
+++ App.tsx
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+} from "react-native";
 
+// SafeAreaView는 아이폰에서 Status Bar 크기 만큼 적용시켜서 padding top같은 역할을 해주는 api
+// 안드로이드에는 적용이 안되기 때문에 Platform과 StatusBar.currentHeight를 사용하여 paddingTop적용
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={{ paddingHorizontal: 30 }}>
+        <View style={styles.inputContainer}>
+          <TextInput style={styles.textInput} />
+          <Button title="클릭" />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  textInput: {
+    width: "70%",
+    borderWidth: 1,
+    borderColor: "#ccc",
+    padding: 8,
+    marginRight: 8,
   },
 });
